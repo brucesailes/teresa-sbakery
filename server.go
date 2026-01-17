59347e43e81d4e6d2158ee25ee0c3a0a74eaaf45
Bruce Sailes <110871245+brucesailes@users.noreply.github.com>
2026-01-16 23:38:08 -0600

    Making a few changes concerning the design.

--- server.go
+++ server.go
@@ -5,6 +5,7 @@ import (
 	"io/fs"
 	"log"
 	"net/http"
+	"os"
 	"path"
 
 	"github.com/gorilla/mux"
@@ -41,7 +42,8 @@ func main() {
 	// Dynamic pages: /pages/about, /pages/menu, etc.
 	r.HandleFunc("/pages/{name}", func(w http.ResponseWriter, r *http.Request) {
 		name := mux.Vars(r)["name"]
-		file := path.Join("tmpl/static/pages", name+".html")
+
+		file := path.Join("tmpl/pages", name+".html")
 
 		data, err := templateFS.ReadFile(file)
 		if err != nil {
@@ -52,6 +54,12 @@ func main() {
 		w.Write(data)
 	})
 
-	log.Println("http://localhost:8000/")
-	log.Fatal(http.ListenAndServe(":8000", r))
+	// ✅ Fly uses PORT env var; default to 8080
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	log.Printf("Listening on http://0.0.0.0:%s/", port)
+	log.Fatal(http.ListenAndServe("0.0.0.0:"+port, r))
 }
